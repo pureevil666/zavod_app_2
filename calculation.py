27dac664c80e2db34e5e604b5d8a97bcc6880c0f
pureevil666 <86472191+pureevil666@users.noreply.github.com>
2022-10-21 14:54:02 +0300

    project

---
+++ calculation.py
@@ -0,0 +1,116 @@
+LETTERS = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя- ()'
+LETTERS = LETTERS + LETTERS.upper()
+group_dict = {}
+overall_score = 0
+label = ''
+
+
+def give_data(data):
+    input_text = data
+    convert_data_to_list(input_text)
+
+
+def convert_data_to_list(data):
+    data_list = []
+    data_list += data.splitlines()
+    for i in range(0, 5):
+        while ' ' * i in data_list:
+            data_list.remove(' ' * i)
+
+    create_groups(data_list)
+
+
+def create_groups(input_list):
+    global group_dict
+    group_list = []
+    for line in input_list:
+        if len(line.strip(LETTERS)) == 0:
+            group_dict[line] = 0
+    for name in group_dict:
+        group_list.append(name)
+    save_indexes(group_dict, input_list, group_list)
+
+
+def save_indexes(group_dict, input_list, group_list):
+    index_list = []
+    for group in group_dict:
+        index = input_list.index(group)
+        index_list.append(index)
+    split_list(index_list, group_dict, input_list, group_list)
+
+
+def split_list(index_list, group_dict, input_list, group_list):
+    for i in range(0, len(index_list)):
+        start = index_list[i]
+        if i < len(index_list) - 1:
+            stop = index_list[i + 1]
+            saved_list = input_list[start + 1:stop]
+        else:
+            saved_list = input_list[start + 1:]
+        group_dict[group_list[i]] = saved_list
+    make_final_dict(group_dict)
+
+
+def make_final_dict(group_dict):
+    for group in group_dict:
+        group_dict[group] = count_result(group_dict[group])
+    count_overall_score(group_dict)
+    create_label(group_dict)
+    group_dict.clear()
+
+
+def count_result(item_list):
+    buffer_count = 0
+    for el in item_list:
+        buffer_list = el.split(' ')
+        if len(buffer_list) == 1 or not buffer_list[1].isdigit():
+            buffer_count += check_value(buffer_list)
+        else:
+            buffer_count += int(buffer_list[1])
+    return buffer_count
+
+
+def count_overall_score(group_dict):
+    global overall_score
+    if overall_score > 0:
+        overall_score = 0
+    for group in group_dict:
+        overall_score += int(group_dict[group])
+
+
+
+def create_label(group_dict):
+    global label
+    if len(label) > 0:
+        label = ''
+    for group in group_dict:
+        label += f'{group}: {group_dict[group]}\n'
+
+
+def check_value(value):
+    new_string = ''
+    output_string = ''
+    for i in value:
+        new_string += i
+    print(new_string)
+    # ---------------------------ЕСЛИ РАЗМЕР ЧИСЛОВОЙ
+    if new_string[0].isdigit() and new_string[1].isdigit():
+        new_string = new_string[2:]
+        print(new_string)
+        for char in new_string:
+            if char.isdigit():
+                output_string += char
+            else:
+                return int(output_string)
+    # -----------------------------ЕСЛИ РАЗМЕР БУКВЕННЫЙ
+    else:
+        for i in range(0, len(new_string)):
+            if i == 0 and new_string[i].isdigit():
+                continue
+            if new_string[i].isdigit():
+                output_string += new_string[i]
+            elif len(output_string) > 0:
+                return int(output_string)
+            else:
+                continue
+        return int(output_string)
---
+++ main.py
@@ -0,0 +1,39 @@
+from kivy.config import Config
+
+Config.set('graphics', 'resizable', 0)
+import calculation
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.core.window import Window
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
+
+Window.size = (400, 600)
+
+
+class MyApp(App):
+    def build(self):
+        layout = GridLayout(rows=2)
+        self.text_input = TextInput()
+        layout.add_widget(self.text_input)
+        layout.add_widget(Button(text='Посчитать', on_press=self.click_button, size_hint=[1, 0.12]))
+        return layout
+
+    def click_button(self, instance):
+        calculation.give_data(self.text_input._get_text())
+        content = GridLayout(cols=1, rows=3, padding=[10])
+        self.popup = Popup(size_hint=(.7, .6), title='Результат', title_align='center',
+                           title_size=20, content=content)
+        self.popup.open()
+        content.add_widget(Label(text=calculation.label))
+        content.add_widget(Label(text=f'Общее: {str(calculation.overall_score)}', font_size=20))
+        content.add_widget(Button(text='Закрыть', on_press=self.close_popup, size_hint=[1, 0.3]))
+
+    def close_popup(self, instance):
+        self.popup.dismiss()
+
+
+if __name__ == '__main__':
+    MyApp().run()
